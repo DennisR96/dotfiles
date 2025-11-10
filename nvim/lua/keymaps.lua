783d1d640b63e0baf76b7a699da079d33a677cca
DennisR96 <dennisraek@gmail.com>
2025-11-10 10:05:42 +0100

    updated nvim config

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,3 +8,6 @@ vim.keymap.set("n", "<leader>b", ":<C-u>WEZSENDBLOCK<CR>", { noremap = true, sil
 vim.keymap.set("n", "<leader>p", ":<C-u>w | !python %<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>q", ":<C-u>q<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>w", ":<C-u>w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory with Oil" })
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
